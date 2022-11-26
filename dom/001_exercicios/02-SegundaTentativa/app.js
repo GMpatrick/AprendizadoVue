b6780f721cfdc6c0f586dda451fbd8637a2dfa0a
GMpatrick <patrickalvessc@gmail.com>
2022-11-26 13:45:00 -0300

    Melhoria dos exercicios feitos

---
+++ dom/001_exercicios/02-SegundaTentativa/app.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#Desafio',
+    data: {
+        nome: 'Patrick eduardo alves',
+        idade: 19,
+        idadeMultTres: 19 * 3,
+        link: 'https://u3r3f6s2.rocketcdn.me/wp-content/uploads/2021/01/image-30-768x443.png.webp'
+    },
+    methods: {
+        numeroRandomico(){
+            return Math.random()
+        }
+    }
+})
+
+
