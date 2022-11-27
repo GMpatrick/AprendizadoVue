19c6c63ae3cdf7f984a954dd46e4a40a85be01a8
GMpatrick <patrickalvessc@gmail.com>
2022-11-27 13:00:02 -0300

    Resolucao de exerciciocios

--- dom/002_exercicios/app.js
+++ dom/002_exercicios/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#desafio',
     data: {
         valor: ''
+    },
+    methods: {
+        alertaBtnClick(event){
+            alert('Você cliclou no botão')
+        },
+        escutarKeydown(event){
+            this.valor = event.target.value
+        }
     }
 })
